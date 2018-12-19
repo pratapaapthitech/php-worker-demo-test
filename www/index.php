a78bd0f0c2bf6b48f5a3a60a2702e5d35e093aa5
Pratap <pjajjara@aapthitech.com>
2018-12-19 12:39:42 +0530

    test

--- www/index.php
+++ www/index.php
@@ -1,50 +1,16 @@
 <?php
 
-use PhpAmqpLib\Message\AMQPMessage;
+// create a new cURL resource
+$ch = curl_init();
 
-$app = require(__DIR__.'/../app.php');
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "https://comp-link.com/rater/api/states");
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-$app->register(new Silex\Provider\TranslationServiceProvider(), [
-    'translator.messages' => [],
-]);
-$app->register(new Silex\Provider\FormServiceProvider());
-$app->register(new Silex\Provider\TwigServiceProvider(), [
-    'twig.path' => __DIR__.'/..',
-    'twig.form.templates' => ['bootstrap_3_layout.html.twig'],
-]);
+// grab URL and pass it to the browser
+curl_exec($ch);
 
-$app->match('/', function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
-    $opinions = $app['predis']->lrange('opinions', 0, 10);
+// close cURL resource, and free up system resources
+curl_close($ch);
 
-    $form = $app['form.factory']->createBuilder('form')
-        ->add('opinion', 'textarea', [
-            'label' => 'Your opinion',
-            'attr' => ['rows' => count($opinions)*2],
-        ])
-        ->getForm();
-    $form->handleRequest($request);
-    $submitted = false;
-    if ($form->isValid()) {
-        $data = $form->getData();
-
-        $connection = $app['amqp']['default'];
-        $channel = $connection->channel();
-        $channel->queue_declare('task_queue', false, true, false, false);
-        
-        $msg = new AMQPMessage($data['opinion'], ['delivery_mode' => 2]);
-        $channel->basic_publish($msg, '', 'task_queue');
-
-        $channel->close();
-        $connection->close();
-
-        $submitted = true;
-    }
-
-    return $app['twig']->render('index.twig', [
-        'form' => $form->createView(),
-        'submitted' => $submitted,
-        'opinions' => $opinions
-    ]);
-});
-
-$app->run();
+?>
